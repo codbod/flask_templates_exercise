83a11a0d4298c173c55df405c54cdda2589dc8ae
Cody Yeung <116022463+codbod@users.noreply.github.com>
2026-02-07 12:10:11 -0700

    added routes for about and contact page
    forgot to add this earlier

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', title="Home Page")
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About Us")
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact Page")
+
 if __name__ == '__main__':
     app.run(debug=True)
